1445031aa2e985fac4463d44e529ea8d0fb12f2d
SangHyunJeong <124690203+SangHyunJeong@users.noreply.github.com>
2023-09-10 01:15:21 +0900

    2강까지.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
-from data import countries_df
+from data import countries_df, totals_df
+from builders import make_table
 
 stylesheets = [
     "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
@@ -11,6 +12,38 @@ stylesheets = [
 ]
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
+
+bubble_map = px.scatter_geo(
+    countries_df,
+    size="Confirmed",
+    size_max=40,
+    title="Confirmed By Country",
+    hover_name="Country_Region",
+    color="Confirmed",
+    template="plotly_dark",
+    color_continuous_scale=px.colors.sequential.Oryel,
+    locations="Country_Region",
+    locationmode="country names",
+    hover_data={
+        "Confirmed": ":,",
+        "Deaths": ":,",
+        "Recovered": ":,",
+        "Country_Region": False,
+    },
+)
+bubble_map.update_layout(margin=dict(l=0, r=0, t=50, b=0))
+
+bars_graph = px.bar(
+    totals_df,
+    x="condition",
+    y="count",
+    hover_data={"count": ":,"},
+    template="plotly_dark",
+    title="Total Global Cases",
+    labels={"condition": "Coundition", "count": "Count"},
+)
+bars_graph.update_layout(xaxis=dict(title="Condition"), yaxis=dict(title="Count"))
+bars_graph.update_traces(marker_color=["#e74c3c", "#8e44ad", "#27ae60"])
 
 app.layout = html.Div(
     style={
@@ -22,40 +55,31 @@ app.layout = html.Div(
     children=[
         html.Header(
             style={"textAlign": "center", "paddingTop": "50px"},
-            children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
+            children=[html.H1("Corona Dashboard", style={"fontSize": 16})],
         ),
         html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
             children=[
                 html.Div(
-                    children=[
-                        html.Table(
-                            children=[
-                                html.Thead(
-                                    children=[
-                                        html.Tr(
-                                            children=[
-                                                html.Th(column_name.replace("_", " "))
-                                                for column_name in countries_df.columns
-                                            ]
-                                        )
-                                    ]
-                                ),
-                                html.Tbody(
-                                    children=[
-                                        html.Tr(
-                                            children=[
-                                                html.Td(value_column)
-                                                for value_column in value
-                                            ]
-                                        )
-                                        for value in countries_df.values
-                                    ]
-                                ),  # 이 부분에서 데이터를 출력해야 합니다.
-                            ]
-                        )
-                    ]
-                )
-            ]
+                    style={"grid-column": "span 3"},
+                    children=[dcc.Graph(figure=bubble_map)],
+                ),
+                html.Div(children=[make_table(countries_df)]),
+            ],
+        ),
+        html.Div(
+            style={
+                "display": "grid",
+                "gap": 50,
+                "gridTemplateColumns": "repeat(4, 1fr)",
+            },
+            children=[
+                html.Div(children=[dcc.Graph(figure=bars_graph)]),
+            ],
         ),
     ],
 )
