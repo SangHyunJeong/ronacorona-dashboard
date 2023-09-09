70884e43a885d8a0bd9e3ccd5411f8cf99b7299d
SangHyunJeong <124690203+SangHyunJeong@users.noreply.github.com>
2023-09-09 17:14:14 +0900

    2023.09.09

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from data import countries_df
+
+stylesheets = [
+    "https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css",
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
+]
+
+app = dash.Dash(__name__, external_stylesheets=stylesheets)
+
+app.layout = html.Div(
+    style={
+        "minHeight": "100vh",
+        "backgroundColor": "#111111",
+        "color": "white",
+        "fontFamily": "Open Sans, sans-serif",
+    },
+    children=[
+        html.Header(
+            style={"textAlign": "center", "paddingTop": "50px"},
+            children=[html.H1("Corona Dashboard", style={"fontSize": 40})],
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Table(
+                            children=[
+                                html.Thead(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Th(column_name.replace("_", " "))
+                                                for column_name in countries_df.columns
+                                            ]
+                                        )
+                                    ]
+                                ),
+                                html.Tbody(
+                                    children=[
+                                        html.Tr(
+                                            children=[
+                                                html.Td(value_column)
+                                                for value_column in value
+                                            ]
+                                        )
+                                        for value in countries_df.values
+                                    ]
+                                ),  # 이 부분에서 데이터를 출력해야 합니다.
+                            ]
+                        )
+                    ]
+                )
+            ]
+        ),
+    ],
+)
+
+if __name__ == "__main__":
+    print(countries_df)  # 이 부분에 print(countries_df)를 추가
+    app.run_server(debug=True)
